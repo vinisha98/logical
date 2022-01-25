0ef35c1e94a146bfdda107b6e3fcb73fe0c72ffc
vishal sam <vinishakr9@gmail.com>
2022-01-25 18:19:26 +0530

    Added prime number logic

---
+++ Prime.java
@@ -0,0 +1,16 @@
+package com.bridfgelabz.logical;
+
+import java.util.Scanner;
+
+import com.bridfgelabz.utility.Utility2;
+
+public class Prime {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);         
+		System.out.print("Enter the number: ");  
+		int Number=sc.nextInt();
+		Utility2.Prime(Number);
+	}
+}
