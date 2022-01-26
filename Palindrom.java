4c19c7627f0736687cc5aa3e72682d83d33c5ef6
vishal sam <vinishakr9@gmail.com>
2022-01-26 22:22:15 +0530

    Added coupon code logic

---
+++ Palindrom.java
@@ -0,0 +1,17 @@
+package com.bridfgelabz.logical;
+
+import java.util.Scanner;
+
+import com.bridfgelabz.utility.Utility2;
+
+public class Palindrom {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number to be reversed: ");
+		int number = sc.nextInt();
+		Utility2.Plaindrom(number);
+	}
+
+}
