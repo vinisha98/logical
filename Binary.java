94a0df0e6eeff55d2eb65932f4b6a6067a2232c6
vishal sam <vinishakr9@gmail.com>
2022-01-27 18:29:58 +0530

    Added binary conversion logic

---
+++ Binary.java
@@ -0,0 +1,17 @@
+package com.bridfgelabz.logical;
+
+import java.util.Scanner;
+
+import com.bridfgelabz.utility.Utility2;
+
+public class Binary {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+	       System.out.println("Enter the number: ");
+	       int num = scan.nextInt();
+	       Utility2.Binary(num); 
+	}
+
+}
